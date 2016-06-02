d968fb6bc4e67659f8840a0e3d3201f15f191b2c
Shazad Ahmed <shazad@co6.co.uk>
2016-06-02 17:22:00 +0100

    Update module.php

--- module.php
+++ module.php
@@ -1,5 +1,39 @@
 <?php
 
+{
+	"color_scheme": "Packages/Color Scheme - Default/Monokai Bright.tmTheme",
+	"draw_white_space": "all",
+	"fade_fold_buttons": false,
+	"font_size": 11,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"indent_guide_options":
+	[
+		"draw_normal",
+		"draw_active"
+	],
+	"line_padding_bottom": 1,
+	"overlay_scroll_bars": "enabled",
+	"show_encoding": true,
+	"show_line_endings": true,
+	"theme": "Boxy Monokai.sublime-theme",
+	"theme_accent_purple": true,
+	"theme_bar": true,
+	"theme_bar_margin_top_sm": true,
+	"theme_button_rounded": true,
+	"theme_find_panel_close_hidden": true,
+	"theme_font_xs": true,
+	"theme_grid_border_size_sm": true,
+	"theme_sidebar_folder_arrow": true,
+	"theme_size_xs": true,
+	"theme_tab_arrows_hidden": true,
+	"theme_tab_font_sm": true,
+	"theme_tab_rounded": true
+}
+
+
 function r_ui_init() {
   global $theme;
   $module_path = drupal_get_path('module', 'r_ui');
